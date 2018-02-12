64b3a8116623b840904d8e2d3bb3e9150073aa8e
MusaBalabekov <33854301+MusaBalabekov@users.noreply.github.com>
2018-02-12 23:24:03 +0600

    first commit

---
+++ HW_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HW_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+            Task2();
+          
+        }
+
+        private static void Task2()
+        {
+            int sum = 0;
+            while (true)
+            {
+                int number = int.Parse(Console.ReadLine());               
+                Console.WriteLine(sum += number);
+            }
+        }
+
+        private static void Task1()
+        {
+            Console.WriteLine("Балабеков Муса");
+            Console.ReadKey();
+        }
+    }
+}
